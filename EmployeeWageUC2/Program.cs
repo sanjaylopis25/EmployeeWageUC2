d24fb72dbe5d308ad134bdd4d54a790ae40a75d1
SANJAY <SANJAY@DESKTOP-OVOTIDF>
2021-10-25 00:11:23 +0100

    Add project files.

---
+++ EmployeeWageUC2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EmployeeWageUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Fulltime = 1;
+            int Emprateperhr = 20;
+
+            int emphrs = 0;
+            int empwage = 0;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == Fulltime)
+            {
+                emphrs = 8;
+            }
+            else
+            {
+                emphrs = 0;
+            }
+            empwage = emphrs * Emprateperhr;
+            Console.WriteLine("Employee Wage : " + empwage);
+        }
+    }
+}
